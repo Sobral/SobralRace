966b1d217873e5d3294bc4843878d1dd1200f48d
Luciano Sobral <sobral.luciano@gmail.com>
2020-03-23 18:46:54 -0400

    Routes implemented and App componente now handle the components routing

--- src/App.js
+++ src/App.js
@@ -1,20 +1,7 @@
 import React from 'react';
-import { Container } from './style';
 
-function App() {
-  return (
-    <Container>
-      <form>
-        <input
-          type="text"
-          id="playerName"
-          placeholder="Nome"
-          autoComplete="off"
-        />
-        <button type="submit">Iniciar Corrida</button>
-      </form>
-    </Container>
-  );
-}
+import Routes from './routes';
+
+const App = () => <Routes />;
 
 export default App;
--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,29 @@
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import React from 'react';
+import { isAuthenticated } from './services/auth';
+import Main from './screens/Main';
+import Game from './screens/Game';
 
-import Main from './pages/Main';
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props =>
+      isAuthenticated() ? (
+        <Component {...props} />
+      ) : (
+        <Redirect to={{ pathname: '/', state: { from: props.location } }} />
+      )
+    }
+  />
+);
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Main} />
+        <PrivateRoute path="/game" exact component={Game} />
+        <Route path="*" component={Main} />
       </Switch>
     </BrowserRouter>
   );
