8767d08d73e890e6d6fedfc596b87f6e31a75c4d
Luciano Sobral <sobral.luciano@gmail.com>
2020-03-24 14:53:27 -0400

    refac[Form] implementing the initial Form
    One of the challenge requisits is that should be exist a form input  to
    type the player name and button for router to game. Now we have a form
    that works fine, and redirect fine too, but lacks style, let keep it as
    it is now and focus in the game instead.

--- src/screens/Main/index.js
+++ src/screens/Main/index.js
@@ -1,15 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { withRouter } from 'react-router-dom';
 import { Container } from './style';
 
-function Main() {
+function Main(props) {
+  const [emptyInput, setEmptyInput] = useState(true);
+  const [newInput, setNewInput] = useState('');
+  const [input, setInput] = useState('');
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    if (!emptyInput) {
+      setInput(newInput);
+      setNewInput('');
+      setEmptyInput(true);
+      props.history.push('/game');
+    }
+  };
+
+  const handleChange = event => {
+    const text = event.target.value;
+    // Checks if the input is empty
+
+    const isInputEmpty = text.trim().length === 0;
+
+    setEmptyInput(isInputEmpty);
+    setNewInput(text);
+  };
+
   return (
     <Container>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
           id="playerName"
           placeholder="Nome"
           autoComplete="off"
+          onChange={handleChange}
+          value={newInput}
         />
         <button type="submit">Iniciar Corrida</button>
       </form>
@@ -17,4 +44,4 @@ function Main() {
   );
 }
 
-export default Main;
+export default withRouter(Main);
