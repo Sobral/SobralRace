e5954cf32b940b2097d3585047d8e7214e47f2be
Luciano Sobral <sobral.luciano@gmail.com>
2020-03-24 23:45:04 -0400

    style[Player] Adding dynamic style into player
    Since we only move along the horizontal axis, we are setting dinamically
    just the left property in the Car position

--- src/features/player/style.js
+++ src/features/player/style.js
@@ -3,10 +3,13 @@ import styled from 'styled-components';
 export const Car = styled.div`
   position: absolute;
   top: 600px;
-  left: 280px;
   width: 200px;
   height: 200px;
   background-image: url('assets/car/car.png');
   background-size: cover;
   background-position: 0 0;
+  left: ${props => {
+    const valor = `${props.position}px`;
+    return valor;
+  }};
 `;
